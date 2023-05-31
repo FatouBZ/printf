9eb162d3c9b7fdfb1833ab37eb2b6205644fcdac
FatouBZ <fbzongo@st.ug.edu.gh>
2023-05-30 18:46:03 -0700

    update

--- printf_HEX_aux.c
+++ printf_HEX_aux.c
@@ -1,5 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
 /**
 * printf_HEX_aux - prints an hexgecimal number.
 * @num: number to print.
@@ -31,40 +30,4 @@ _putchar(array[i] + '0');
 }
 free(array);
 return (counter);
-=======
-
-/**
- * printf_HEX_aux - prints an hexadecimal number.
- * @num: number to print.
- * Return: counter.
- */
-int printf_HEX_aux(unsigned int num)
-{
-	int j;
-	int *array;
-	int counter = 0;
-	unsigned int temp = num;
-
-	while (num / 16 != 0)
-	{
-		num /= 16;
-		counter++;
-	}
-	counter++;
-	array = malloc(counter * sizeof(int));
-
-	for (j = 0; j < counter; j++)
-	{
-		array[j] = temp % 16;
-		temp /= 16;
-	}
-	for (j = counter - 1; j >= 0; j--)
-	{
-		if (array[j] > 9)
-			array[j] = array[j] + 7;
-		_putchar(array[j] + '0');
-	}
-	free(array);
-	return (counter);
->>>>>>> 383f42488eec0d92e386acb440074cfafbe4ef1c
 }
